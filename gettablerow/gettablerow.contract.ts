c778a1fdb2df4666deb425efad175ffee193e16a
Remy Chauveau <rem.chauveau@gmail.com>
2024-01-11 16:19:21 +0100

    Fix import path from external

--- gettablerow/gettablerow.contract.ts
+++ gettablerow/gettablerow.contract.ts
@@ -1,8 +1,7 @@
-import { Name, Contract, TableStore } from "proton-tsc"
+import { Name, Contract, TableStore, print } from "proton-tsc"
 import { ATOMICASSETS_CONTRACT, Assets } from 'proton-tsc/atomicassets'
-
-// import { ATOMICMARKET_CONTRACT } from "external/atomicmarket/atomicmarket.constants.ts"
-// import { Auctions } from "external/atomicmarket/atomicmarket.tables.ts"
+import { ATOMICMARKET_CONTRACT } from "./../external/atomicmarket/atomicmarket.constants"
+import { Auctions } from "./../external/atomicmarket/atomicmarket.tables"
 
 @contract
 class GetTableRowExample extends Contract {
@@ -14,7 +13,8 @@ class GetTableRowExample extends Contract {
         const assetId: u64 = 1
         const asset = new TableStore<Assets>(ATOMICASSETS_CONTRACT, testaccount).requireGet(assetId, 'asset not found')
 
-        // const auctionId: u64 = 1
-        // const auction = new TableStore<Auctions>(ATOMICMARKET_CONTRACT).requireGet(auctionId, 'auction not found')
+         const auctionId: u64 = 1
+        const auction = new TableStore<Auctions>(ATOMICMARKET_CONTRACT).requireGet(auctionId, 'auction not found')
+        
     }
 }
